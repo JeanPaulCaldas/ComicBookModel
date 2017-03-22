bd5eb870be4f17269192d3559302e55984a20a82
Milenas Cruz <jeancaldas.sena@gmail.com>
2017-03-22 00:12:36 -0500

    Creación del proyecto, ejercicio de integración con EF

---
+++ src/ComicBookModel/Models/ComicBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookModel.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
